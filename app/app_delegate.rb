8d7544c37ade8d0e2ebc4c1caea0f3bf70dc6080
Morgan Schweers <cyberfox@gmail.com>
2017-09-22 03:07:05 -0700

    Create the status bar menu. The icon still doesn't work. :(

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,12 +4,36 @@ class AppDelegate
     'TakePictures' => NSOnState
   }
 
+  def createMenuItem(name, action, key='', target=self)
+    NSMenuItem.alloc.initWithTitle(name, action: action, keyEquivalent: key).tap do |item|
+      item.target = target unless target.nil?
+    end
+  end
+
   def applicationDidFinishLaunching(notification)
     NSUserDefaults.standardUserDefaults.registerDefaults PREFS_DEFAULTS
     @tasks = Tasks.new
     @prompter = Prompter.new(@tasks)
 
-    buildMenu
+#    buildMenu
+    appName = NSBundle.mainBundle.infoDictionary['CFBundleName']
+    @appmenu ||= NSMenu.new.tap do |menu|
+      menu.initWithTitle 'Whazzup!'
+      menu.addItem NSMenuItem.separatorItem
+      menu.addItem createMenuItem('Ask', 'askEarly', '!')
+      menu.addItem createMenuItem("About #{appName}", 'orderFrontStandardAboutPanel:')
+      menu.addItem NSMenuItem.separatorItem
+      menu.addItem createMenuItem('Preferences', 'openPreferences:', ',')
+      sparkle = createMenuItem("Check for updates...", nil)
+      menu.addItem sparkle
+      sparkle.setTarget SUUpdater.new
+      sparkle.setAction 'checkForUpdates:'
+      menu.addItem NSMenuItem.separatorItem
+      menu.addItem createMenuItem('Show in Finder', 'finderView', 'i')
+      menu.addItem NSMenuItem.separatorItem
+      menu.addItem createMenuItem("Quit #{appName}", 'terminate:', 'q')
+    end
+
     @status_item = NSStatusBar.systemStatusBar.statusItemWithLength(NSVariableStatusItemLength).init.tap do |statusbar|
       statusbar.setMenu @appmenu
       statusbar.setImage(NSImage.imageNamed('179-notebook.png'))
